e22e1b9c8c6682bf4f59c5a9b8399af152ce83e9
anirudh goyal <59086726+anirudh-maker@users.noreply.github.com>
2020-02-04 16:23:56 +0530

    Add files via upload

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -5,9 +5,9 @@ $email_subject = 'new form';
 $email_body = "user name:$name.\n";
 $to="goyal.anirudh7@gmail.com";
 $headers="From:$email_from\r\n";
-$headers="Reply-To:$visitor_email\r\n";
+
 mail($to,$email_subject,$email_body,$headers);
-header("location:test.html");
+header("location: test.html");
 
 ?>
 
